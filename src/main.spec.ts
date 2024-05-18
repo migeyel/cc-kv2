59fe74135b55858c96c42a71aba6899d51f28f2f
Miguel Oliveira <miguel.oliveira4224@gmail.com>
2024-05-18 19:00:11 -0300

    Let test code specify the tested module

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,23 +1,23 @@
-export function exit(returnCode?: number) {
-    // @ts-expect-error: CraftOS-PC allows shutting down with a return code.
-    os.shutdown(returnCode);
-}
+const arg: string | undefined = [...$vararg][0];
+const arg_module = arg && "src." + arg + ".spec";
+
+assert(arg_module);
 
 // @ts-expect-error: This is a dirty hack to allow module introspection.
 const modules: LuaTable<string, unknown> = ____modules;
 
 for (const [module] of modules) {
-    if (string.find(module, "%.spec$")[0] && module != "main.spec") {
-        io.stdout.write(module + "\n");
-        const [ok, err] = xpcall(() => require(module), (m) => {
-            return debug.traceback(coroutine.running()[0], m, 2);
-        });
+    if (string.find(module, "%.spec$")[0] && module != "src.main.spec") {
+        if (!arg_module || arg_module == module) {
+            io.stdout.write(module + "\n");
+            const [ok, err] = xpcall(() => require(module), (m) => {
+                return debug.traceback(coroutine.running()[0], m, 2);
+            });
 
-        if (!ok) {
-            io.stderr.write(err + "\n");
-            exit(1);
+            if (!ok) {
+                printError(tostring(err) + "\n");
+                throw err;
+            }
         }
     }
 }
-
-exit(0);
